4929c5b68999037cf1ac02e2d764063501a21a20
Cary Robbins <carymrobbins@gmail.com>
2018-08-01 15:03:27 -0500

    Determine extensions from content and/or -X

--- src/Language/Haskell/GHC/Parser/Main.hs
+++ src/Language/Haskell/GHC/Parser/Main.hs
@@ -1,26 +1,32 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Language.Haskell.GHC.Parser.Main where
 
 import Language.Haskell.GHC.Parser.Internal.JSON
 
-import Control.Monad.Trans (liftIO)
+import Control.Monad.Trans (MonadIO, liftIO)
 import Data.Char
 import Data.IORef
-import Data.List (intercalate)
+import Data.List (intercalate, isPrefixOf)
 import Data.List.Split (splitOn)
 import Data.Maybe
 import System.Console.Haskeline
 import System.Directory
 import System.IO
 import System.IO.Unsafe
+import System.Environment
+import Text.Read (readMaybe)
 
 import DynFlags
 import FastString
 import GHC
 import GHC.Paths
+import GHC.LanguageExtensions (Extension)
+import qualified GHC.LanguageExtensions
 import Lexer
 import SrcLoc
 import StringBuffer
-import System.Environment
 
 main :: IO ()
 main = do
@@ -33,9 +39,9 @@ main = do
     Just c -> runCommand c
 
   runCommand c = case break (== ' ') c of
-    ("", "")          -> loop
-    ("lex", arg)      -> lexCmd runLazyLexer arg
-    ("lexall", arg)   -> lexCmd runStrictLexer arg
+    ("", "") -> loop
+    ("lex", argStr) -> withParseArgsIO argStr $ lexCmd runLazyLexer
+    ("lexall", argStr) -> withParseArgsIO argStr $ lexCmd runStrictLexer
     (q, _) | isQuit q -> return ()
     (cmd, _) -> do
       outputStrLn $ "Unknown command: " ++ cmd
@@ -43,56 +49,112 @@ main = do
 
   isQuit q = q `elem` ["quit", "q", "exit"]
 
-  lexCmd f arg = case trim arg of
-    ""   -> liftIO (lexStdin f) >> loop
-    file -> do
+  lexCmd f args = case argsFile args of
+    Nothing -> liftIO (lexStdin f args) >> loop
+    Just file -> do
       x <- liftIO $ doesFileExist file
-      if x then liftIO (lexFile f file) else outputStrLn $ "File not found: " ++ file
+      if x then liftIO (lexFile f file args) else outputStrLn $ "File not found: " ++ file
       loop
 
-  lexFile f file = do
+  lexFile f file args = do
     contents <- readFile file
     let stringBuf =
             stringToStringBuffer
           $ intercalate "\n"
           $ map applyCommentCPPToLine
           $ splitOn "\n" contents
-    f stringBuf
+    f stringBuf args
 
-  lexStdin f = do
+  lexStdin f args = do
     stringBuf <- consumeStdin
-    f stringBuf
+    f stringBuf args
+
+data Args = Args
+  { argsFile :: Maybe FilePath
+  , argsExts :: [Extension]
+  }
+
+withParseArgsIO :: MonadIO io => String -> (Args -> io ()) -> io ()
+withParseArgsIO argStr f = either (liftIO . putStrLn) f $ parseArgs argStr
+
+parseArgs :: String -> Either String Args
+parseArgs argStr = loop argStr $ Args Nothing []
+  where
+  loop s args = case s of
+    c : rest | isSpace c -> loop rest args
+    c : rest | c `elem` ['"', '\''] -> parseFile (Just c) rest args
+    '-' : 'X' : rest -> parseExt rest args
+    "" -> Right args
+    rest -> parseFile Nothing rest args
+
+  parseFile maybeQuote s args = case maybeQuote of
+    Just q -> case break (== q) s of
+      (file, "") -> Left $ "Unterminated string: " ++ (q : file)
+      (file, _ : rest) -> continueWithArgsFile args file rest
+    Nothing -> case break isSpace s of
+      (file, rest) -> continueWithArgsFile args file rest
+
+  continueWithArgsFile args file rest = case argsFile args of
+    Just _ -> Left $ "File already supplied, unexpected argument: " ++ file
+    Nothing | null file -> Left "Invalid file: empty string"
+    Nothing -> loop rest args { argsFile = Just file }
+
+  parseExt s args = case readMaybe s of
+    Nothing -> Left $ "Invalid extension: " ++ s
+    Just x -> Right args { argsExts = x : argsExts args }
 
 myHaskelineSettings :: System.Console.Haskeline.Settings IO
 myHaskelineSettings = defaultSettings { historyFile = Just $ homeDir ++ "/.ghc-lexer-history" }
+
+decodeLangPragma :: Located Token -> [Extension]
+decodeLangPragma (L _ tok) = case tok of
+  ITblockComment s -> decode s
+  _ -> []
+  where
+  decode s =
+    if isLangPragma then mapMaybe readMaybe extStrings else []
+    where
+    isLangPragma :: Bool
+    isLangPragma = any (`isPrefixOf` map toLower s) ["{-# language ", "{-#language"]
+
+    extStrings :: [String]
+    extStrings =
+        map (filter (/= ','))
+      $ filter (\x -> map toLower x `notElem` ["{-#", "language", "{-#language", "#-}"])
+      $ words s
+
+-- Standalone deriving instance so we can easily decode Extensions from Strings.
+deriving instance Read Extension
 
 {-# NOINLINE globalDynFlags #-}
 globalDynFlags :: DynFlags
 globalDynFlags = unsafePerformIO $ runGhc (Just libdir) $ return unsafeGlobalDynFlags
 
 defaultFlags :: DynFlags
-defaultFlags =
-    flip gopt_set Opt_KeepRawTokenStream
-  . flip gopt_set Opt_Haddock
-  $ globalDynFlags
+defaultFlags = foldl gopt_set globalDynFlags [Opt_KeepRawTokenStream, Opt_Haddock]
 
 initSrcLoc :: RealSrcLoc
 initSrcLoc = mkRealSrcLoc (mkFastString "a.hs") 1 1
 
-runLazyLexer :: StringBuffer -> IO ()
-runLazyLexer stringBuf = do
+runLazyLexer :: StringBuffer -> Args -> IO ()
+runLazyLexer stringBuf args = do
   putStrLn "OUTPUT: (Press enter for next element. Anything else will stop lexing)"
-  runLexer (("" ==) <$> getLine) stringBuf
+  runLexer (("" ==) <$> getLine) stringBuf args
 
-runStrictLexer :: StringBuffer -> IO ()
+runStrictLexer :: StringBuffer -> Args -> IO ()
 runStrictLexer = runLexer (return True)
 
-runLexer :: IO Bool -> StringBuffer -> IO ()
-runLexer shouldContinue stringBuf = do
-  let initialState = mkPState defaultFlags stringBuf initSrcLoc
+runLexer :: IO Bool -> StringBuffer -> Args -> IO ()
+runLexer shouldContinue stringBuf Args {..} = do
   pStateRef <- newIORef initialState
   loop pStateRef
   where
+  allExts = argsExts ++ obtainExtensions stringBuf
+
+  initialFlags = updateDynFlagExtensions allExts defaultFlags
+
+  initialState = mkPState initialFlags stringBuf initSrcLoc
+
   loop pStateRef = shouldContinue >>= \case
     True  -> showNextToken pStateRef
     False -> return ()
@@ -103,10 +165,20 @@ runLexer shouldContinue stringBuf = do
       PFailed srcSpan msgDoc -> putJSONLn $ Failure msgDoc srcSpan
       POk pState' tok -> do
         writeIORef pStateRef pState'
+        -- writeIORef pStateRef $ adjustState pState' tok
         putJSONLn $ locTokenToJSON tok
         case tok of
           L _ ITeof -> return ()
           L _ _     -> loop pStateRef
+
+obtainExtensions :: StringBuffer -> [Extension]
+obtainExtensions stringBuf =
+  case lexTokenStream stringBuf initSrcLoc defaultFlags of
+    POk _ tokens -> tokens >>= decodeLangPragma
+    _ -> []
+
+updateDynFlagExtensions :: [Extension] -> DynFlags -> DynFlags
+updateDynFlagExtensions exts dflags = foldl xopt_set dflags exts
 
 {-# NOINLINE _STDIN_EOF #-}
 _STDIN_EOF :: String
